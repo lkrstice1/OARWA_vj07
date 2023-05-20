43c7bda14b85616e478cea8947d248130d519fca
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-05-20 19:04:56 +0200

    Kraj

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+/*
 let poruke = [
     {
       id: 1,
@@ -15,11 +15,13 @@ let poruke = [
       sadrzaj: 'GET i POST su najvaznije metode HTTP protokola',
       vazno: true
     }
-  ]
+  ]*/
   
   const express = require('express')
   const cors = require('cors')
   const app = express()
+  const Poruka = require('./models/poruka')
+const poruka = require('./models/poruka')
   app.use(express.static('build'))
   app.use(cors())
   app.use(express.json())
@@ -29,30 +31,35 @@ let poruke = [
     res.send('<h1>Pozdrav od Express servera!</h1>')
   })
   
-  app.get('/api/poruke', (req, res) =>{
-    res.json(poruke)
-  })
+  app.get('/api/poruke', (req, res) =>
+{
+ Poruka.find({}).then( rezultat =>
+{
+ res.json(rezultat)
+ })
+})
 
   app.get('/api/poruke/:id', (req, res) =>{
-    const id = Number(req.params.id)
-    const poruka = poruke.find(p => p.id === id)
-    
-    if (poruka){
-      res.json(poruka)
-    } else {
-      res.status(404).end()
-    }
+    Poruka.findById(req.params.id)
+    .then(poruka => {
+      if(poruka) {
+        res.json(poruka)
+      } else{
+        res.status(404).end()
+      }
+    })
+    .catch(err => next(err))
   })
 
   app.delete('/api/poruke/:id', (req, res) => {
-    const id = Number(req.params.id)
-    console.log("Brisem poruku sa ID:", id);
-    //poruke = poruke.filter(p => p.id !== id)
-  
-    res.status(204).end()
+    Poruka.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(err => next(err))
   })
   
-  app.post('/api/poruke', (req, res) => {
+  app.post('/api/poruke', (req, res, next) => {
 
     const podatak = req.body
     if(!podatak.sadrzaj){
@@ -65,21 +72,28 @@ let poruke = [
       sadrzaj: podatak.sadrzaj,
       vazno: podatak.vazno || false,
       datum: new Date(),
-      id: generirajId()
     }
-    poruke = poruke.concat(poruka)
-    
-    res.json(poruka)
+    poruka.save().then(spremljenaPoruka => {
+      res.json(spremljenaPoruka)
+    })
+    .catch(err => next(err))
   })
 
   app.put('/api/poruke/:id', (req, res) => {
 
     const podatak = req.body
     const id = Number(req.params.id)
-    console.log("Promjena važnosti poruke sa ID", id)
-    poruke = poruke.map(p => p.id !== id ? p : podatak)
-    console.log(poruke)   
-    res.json(podatak)
+
+    const poruka = {
+      sadrzaj: podatak.sadrzaj,
+      vazno: podatak.vazno
+    }
+
+    Poruka.findByIdAndUpdate(id, poruka, {new: true})
+    .then(novaPoruka => {
+      res.json(novaPoruka)
+    })
+    .catch(err => next(err))
   })
 
   const generirajId = () => {
--- models/poruka.js
+++ models/poruka.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const password = process.env.ATLAS_PASS
 console.log(password);
@@ -6,9 +7,40 @@ const dbname = 'poruke-api'
 const url = `mongodb+srv://lucijakrsticevic:${password}@cluster0.qpriudf.mongodb.net/${dbname}?retryWrites=true&w=majority`
 
 const porukaSchema = new mongoose.Schema({
-    sadrzaj: String,
-    datum: Date,
-    vazno: Boolean
+    sadrzaj: {
+        type: String,
+        minlength: 5,
+        required: true
+    },
+    datum:{
+        type: Date,
+        required: true
+    },
+    vazno: {
+        type: Boolean,
+        default: false
+    }
+})
+
+porukaSchema.set('toJSON', {
+    transform: (doc, ret) => {
+        ret.id = doc._id.toString()
+        delete ret._id
+        delete ret.__v
+        return ret
+    }
 })
 
 const Poruka = mongoose.model('Poruka', porukaSchema, 'poruke')
+
+module.exports = mongoose.model('Poruka', porukaSchema, 'poruke')
+
+console.log("Spajamo se na bazu")
+
+mongoose.connect(url)
+.then(result => {
+    console.log("Spojeni smo na bazu");
+})
+.catch(error => {
+    console.log("Greška pri spajanju", error.message);
+})
